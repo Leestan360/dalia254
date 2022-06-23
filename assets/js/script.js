9024c6d281b88c23e21488d5483d5cf1344475cd
Leestan360 <stanleymayore2@gmail.com>
2022-06-23 12:37:21 +0300

    Update style.css file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
     e.preventDefault();
     console.log("The DOM has loaded");
 });
+
+const dataAPI = http
+
+function fetchData(data) {
+    fetch("http://localhost:3000/upcomingEvents")
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
