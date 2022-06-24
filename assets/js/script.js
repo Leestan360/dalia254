3c19d1d0d28fdc725adc5487dc19848a6c6bfca2
Leestan360 <stanleymayore2@gmail.com>
2022-06-24 17:25:50 +0300

    Finish signup with javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
     e.preventDefault();
 });
 
+// API data
 const upcomingEventsData = "https://dahlia254-server.herokuapp.com/upcomingEvents";
 const weddingsData = "https://dahlia254-server.herokuapp.com/weddings";
 const usersData = "https://dahlia254-server.herokuapp.com/users";
@@ -12,15 +13,32 @@ const signUpButton = document.getElementById("btn2");
 const signUpRedirect = document.getElementById("signOn");
 const logInRedirect = document.getElementById("logOn")
 
+//signup
+const form = document.getElementById("sign-up");
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
+const password = document.getElementById("userPassword");
+const confirmPassword = document.getElementById("confirmPassword1");
+const submitButton = document.getElementById("submit")
+
+
+// Display login form
 loginButton.addEventListener("click", () => {
     const signIn = document.getElementById("signIn");
     signIn.style.display = "block";
 });
 
+
+//Display signup form
 signUpButton.addEventListener("click", () => {
     const signUp = document.getElementById("register");
     signUp.style.display = "block";
 });
+
+function renderLogin() {
+
+}
+
 
 function fetchData() {
     fetch(upcomingEventsData)
@@ -38,12 +56,34 @@ function fetchWedding() {
 
 fetchWedding()
 
-function fetchUser() {
-    fetch(usersData)
+function fetchUser(user1) {
+    fetch(usersData, {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(user1)
+    })
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(user => console.log(user))
 }
 
 fetchUser()
 
 
+// Signup
+
+setInterval(() => {
+    if (userName.value == "" || userEmail.value == "" 
+    || password.value == "" || confirmPassword == "") {
+        submitButton.setAttribute("disabled", "");
+    } else {
+        submitButton.removeAttribute("disabled");
+    }
+})
+
+submitButton.onclick = function () {
+    window.location.href = "../../index.html";
+    var username = userName.value;
+    alert("Welcome" + " " + userName.value);
+}
