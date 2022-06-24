723d3e9895a8e0a489a8ed70b4afc526d294a29b
Leestan360 <stanleymayore2@gmail.com>
2022-06-24 09:58:50 +0300

    Finish signup page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", (e) => {
     e.preventDefault();
-    console.log("The DOM has loaded");
 });
 
 const upcomingEventsData = "https://dahlia254-server.herokuapp.com/upcomingEvents";
@@ -11,7 +10,30 @@ const eventImage = document.getElementById("image");
 const loginButton = document.getElementById("btn1");
 const signUpButton = document.getElementById("btn2");
 
-// loginButton.addEventListener("click", () => {
-//     const userName = 
-// })
+loginButton.onclick = function() {
+    document.getElementsById("signUp").style.display = "block";
+}
 
+function fetchData() {
+    fetch(upcomingEventsData)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+fetchData()
+
+function fetchWedding() {
+    fetch(upcomingEventsData)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+fetchWedding()
+
+function fetchUser() {
+    fetch(usersData)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+fetchUser()
